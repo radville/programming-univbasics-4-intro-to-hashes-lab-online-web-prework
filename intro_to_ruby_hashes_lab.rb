760a6389dcdff6298b263227d732aa28a3ae5b98
Laura Radville <laura.radville@gmail.com>
2019-06-21 12:02:17 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,12 +22,12 @@ end
 
 def my_hash_creator(key, value)
   hash = {
-    key +.
+    key => value
   }
 end
 
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
+  
 end
 
 def update_counting_hash(hash, key)
